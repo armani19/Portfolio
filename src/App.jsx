d3384ab07616fefb54f8beccc7f545ca2f645ff5
armani19 <armanimagnifico4195@gmail.com>
2026-08-03 17:24:36 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <div>
+    <header style={{ 
+      background: "#305982", 
+      padding: "1%", 
+      color: "white",
+      fontSize: "25px",
+      paddingLeft: "10%",
+      borderBottomLeftRadius: "20px",
+      borderBottomRightRadius: "20px",
+      fontFamily: "'Archivo Black', sans-serif"
+      }}>
+
+      <h3 style={{margin:0}}>Magnifico |
+        <span style={{
+          fontSize: "20px",
+          marginLeft: "10px",
+        }}> BSIT-WMA Aspirant</span>
+      </h3>
+    </header>
+    
+    <main style={{ padding: "1%" }}>
+        <h2>Hello!</h2>
+        <p style={{fontFamily: "'Onest', sans-serif"}}>Welcome to my portfolio</p>
+    </main>
+    </div>
+  );
+}
+
+export default App;
